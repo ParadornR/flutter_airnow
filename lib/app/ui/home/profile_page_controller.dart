a6761933f25d14668e8022762836d021035a8d2e
intro5-mix <paradorn.rinrit63@gmail.com>
2025-04-08 14:17:18 +0700

    [mix]"add controller"

---
+++ lib/app/ui/home/profile_page_controller.dart
@@ -0,0 +1,19 @@
+import 'dart:developer';
+
+import 'package:flutter_airnow/app/UI/login/login_page.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ProfilePageController extends GetxController {
+  Future<void> logOut() async {
+    // clear data and jump to login Screen
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    log('${prefs.getBool('isLogin')}');
+    log('${prefs.getString('uid')}');
+    await prefs.remove('isLogin');
+    await prefs.remove('uid');
+    log('isLogin:${prefs.getBool('isLogin')}');
+    log('uid:${prefs.getString('uid')}');
+    Get.off(() => LoginPage());
+  }
+}
