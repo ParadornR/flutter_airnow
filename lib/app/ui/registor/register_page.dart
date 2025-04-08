8f5fbbc2050f05eab294168da63a1d2513cc2321
intro5-mix <paradorn.rinrit63@gmail.com>
2025-04-08 11:29:07 +0700

    [mix]"add sign up"

--- lib/app/ui/registor/register_page.dart
+++ lib/app/ui/registor/register_page.dart
@@ -1,5 +1,7 @@
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_airnow/app/ui/widget/custom_text.dart';
 
@@ -11,6 +13,55 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // collection users
+  final users = FirebaseFirestore.instance.collection('users');
+
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController cfPasswordController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Future<void> _register() async {
+    if (passwordController.text == cfPasswordController.text) {
+      if (_formKey.currentState!.validate()) {
+        try {
+          UserCredential userCredential = await _auth
+              .createUserWithEmailAndPassword(
+                email: emailController.text.trim(),
+                password: passwordController.text.trim(),
+              );
+          // ดึง UID ของผู้ใช้
+          String uid = userCredential.user!.uid;
+          log("User signUp UID: $uid");
+          // collection users
+          users.doc(uid).set({
+            'name': fullNameController.text,
+            'email': emailController.text,
+            // 'profile_picture': urlController.text,
+            'created_at': DateTime.now(),
+          });
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text("สมัครสมาชิกสำเร็จ!")));
+          Navigator.pop(context); // กลับไปหน้าก่อนหน้า (Login)
+        } catch (e) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text("เกิดข้อผิดพลาด: $e")));
+        }
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("The password and confirmation password do not match."),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -31,68 +82,133 @@ class _RegisterPageState extends State<RegisterPage> {
                 end: Alignment.topLeft,
               ),
             ),
-            child: Column(
-              children: [
-                Spacer(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: CustomText(
-                    text: "Create Account",
-                    size: 36,
-                    weight: FontWeight.w700,
-                  ),
-                ),
-                SizedBox(height: 8),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Spacer(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CustomText(
-                      text: "Don’t have an account?",
-                      size: 16,
-                      weight: FontWeight.w200,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Spacer(),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: CustomText(
+                      text: "Create Account",
+                      size: 36,
+                      weight: FontWeight.w700,
                     ),
-                    TextButton(
-                      onPressed: () {
-                        log('Sign up');
-                      },
-                      child: CustomText(
-                        text: "Sign up",
-                        size: 16,
-                        weight: FontWeight.w600,
+                  ),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    controller: fullNameController,
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      labelText: "Full Name",
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your full name.";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.mail),
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your full name.";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: "Password",
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.length < 6) {
+                        return "Please enter your password.";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: cfPasswordController,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: "Confirm Password",
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.length < 6) {
+                        return "Please enter your confirm password.";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          _register();
+                        },
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text("SIGN UP"),
+                            SizedBox(width: 8),
+                            Icon(Icons.arrow_forward),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomText(
+                        text: "Already have a accont?",
+                        size: 16,
+                        weight: FontWeight.w200,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          log('Sign up');
+                          Navigator.pop(context);
+                        },
+                        child: CustomText(
+                          text: "Sign in",
+                          size: 16,
+                          weight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
