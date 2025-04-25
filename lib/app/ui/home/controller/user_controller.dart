520d41e40900ba39172544bac18c67ddfd0b763f
intro5-mix <paradorn.rinrit63@gmail.com>
2025-04-25 11:20:54 +0700

    [mix]"add up dadate function"

--- lib/app/ui/home/controller/user_controller.dart
+++ lib/app/ui/home/controller/user_controller.dart
@@ -1,6 +1,11 @@
 import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_airnow/app/data/models/city_data_model.dart';
 import 'package:flutter_airnow/app/data/models/user_model.dart';
+import 'package:flutter_airnow/app/data/providers/city_data_provider.dart';
+import 'package:flutter_airnow/app/data/repositories/city_data_repository.dart';
+import 'package:flutter_airnow/app/ui/home/home_page.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -14,11 +19,11 @@ class UserController extends GetxController {
 
   @override
   void onInit() {
+    cityDataRepository = CityDataRepository(apiService: CityDataApiService());
     fetchUserData();
-    fetchAllCardLocationData();
     super.onInit();
   }
-  
+
   // Function to fetch user data and update the state
   Future<void> fetchUserData() async {
     try {
@@ -30,6 +35,7 @@ class UserController extends GetxController {
         if (snapshot.exists) {
           user.value = User.fromMap(snapshot.data() as Map<String, dynamic>);
           log('[fetchUserData] Data: ${user.value}');
+          fetchAllCardLocationData();
         } else {
           log('[fetchUserData] No user found');
         }
@@ -42,18 +48,59 @@ class UserController extends GetxController {
   }
 
   Future<void> deleteCardDataUser(String city) async {
-    
     try {
+      // เรียกดู userId จาก SharedPreferences
       userId.value = (await _getUserIdFromSharedPreferences());
       log('[deleteCardDataUser] userId : $userId');
+
       if (userId.value != null) {
+        // ลบเอกสารทั้งหมดในคอลเล็กชัน 'weather'
+        final weatherDocs =
+            await firestore
+                .collection('users')
+                .doc(userId.value!)
+                .collection('location')
+                .doc(city)
+                .collection('weather')
+                .get();
+
+        for (var doc in weatherDocs.docs) {
+          await doc.reference.delete();
+        }
+
+        // ลบเอกสารทั้งหมดในคอลเล็กชัน 'pollution'
+        final pollutionDocs =
+            await firestore
+                .collection('users')
+                .doc(userId.value!)
+                .collection('location')
+                .doc(city)
+                .collection('pollution')
+                .get();
+
+        for (var doc in pollutionDocs.docs) {
+          await doc.reference.delete();
+        }
+
+        // หลังจากลบเอกสารในคอลเล็กชัน 'weather' และ 'pollution' แล้ว ลบเอกสาร 'Bang Kapi'
         await firestore
             .collection('users')
             .doc(userId.value!)
             .collection('location')
             .doc(city)
             .delete();
-        Get.back();
+        Get.snackbar(
+          'Success',
+          "The item has been successfully deleted.",
+          margin: EdgeInsets.all(8),
+          backgroundColor: Color.fromRGBO(76, 175, 80, 1),
+          colorText: Colors.white,
+        );
+        // กลับไปที่หน้าก่อน
+        Get.offAll(() => HomePage());
+        log(
+          'Successfully deleted weather, pollution documents and the city document.',
+        );
       } else {
         log('[deleteCardDataUser] CardData is not available');
       }
@@ -112,9 +159,9 @@ class UserController extends GetxController {
       final results = await Future.wait([weatherFuture, pollutionFuture]);
 
       final weather =
-          results[0].docs.isNotEmpty ? results[0].docs.first.data() : {};
+          results[0].docs.isNotEmpty ? results[0].docs.last.data() : {};
       final pollution =
-          results[1].docs.isNotEmpty ? results[1].docs.first.data() : {};
+          results[1].docs.isNotEmpty ? results[1].docs.last.data() : {};
 
       fullData.add({
         'location': locationData,
@@ -129,5 +176,115 @@ class UserController extends GetxController {
   Future<String?> _getUserIdFromSharedPreferences() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('uid');
+  }
+
+  late CityDataRepository cityDataRepository;
+  var cityData = Rx<CityDataModel?>(null);
+
+  updateData() async {
+    final length = dataList.length;
+    log("[dataList]: $dataList");
+    log("[length]: $length");
+    for (var index = 0; index <= length; index++) {
+      final city = dataList[index]['location']['city'];
+      final state = dataList[index]['location']['state'];
+      final country = dataList[index]['location']['country'];
+      await loadDataCityWithCSC(city: city, state: state, country: country);
+      await loadAndUpdate();
+    }
+    await fetchAllCardLocationData();
+  }
+
+  Future<void> loadDataCityWithCSC({
+    required String city,
+    required String state,
+    required String country,
+  }) async {
+    log('[loadDataCityWithCSC]: loading...');
+    log('[CSC][city]: $city, [state]: $state, [country]: $country');
+    try {
+      final result = await cityDataRepository.fetchDataWithCSC(
+        city,
+        state,
+        country,
+      );
+      cityData.value = result;
+      log('[loadDataCityWithCSC]: succeed');
+      log('[cityData.value]: ${cityData.value!.data.city}');
+      // log('[cityData.value]: ${cityData.value}');
+    } catch (e) {
+      log("[loadDataCityWithCSC][Error]$e");
+    }
+  }
+
+  loadAndUpdate() async {
+    final value = cityData.value!.data;
+    final pollution = cityData.value!.data.current.pollution;
+    final weather = cityData.value!.data.current.weather;
+
+    try {
+      final locationRef = FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId.value)
+          .collection('location');
+      final cityDocRef = locationRef.doc(value.city);
+
+      // 2. ตรวจสอบการซ้ำใน pollutionRef
+      final pollutionRef = cityDocRef.collection('pollution');
+      final pollutionQuery =
+          await pollutionRef
+              .where(
+                'ts',
+                isEqualTo: pollution.ts,
+              ) // ตรวจสอบว่า timestamp ซ้ำไหม
+              .get();
+
+      if (pollutionQuery.docs.isEmpty) {
+        // ไม่มีข้อมูล pollution นี้ ซ้ำกันแล้ว
+        log("[pollution.ts]: ${pollution.ts}");
+        await pollutionRef.add({
+          'aqicn': pollution.aqicn,
+          'aqius': pollution.aqius,
+          'maincn': pollution.maincn,
+          'mainus': pollution.mainus,
+          'ts': pollution.ts,
+          'created_at': DateTime.now(),
+        });
+        log("[pollution.ts][${value.city}]:✔️ ดึงและบันทึกข้อมูลเสร็จแล้ว");
+      } else {
+        log(
+          "[loadAndUpdate][${value.city}]: ❌ This pollution data is repeated.",
+        );
+      }
+
+      // 3. ตรวจสอบการซ้ำใน weatherRef
+      final weatherRef = cityDocRef.collection('weather');
+      final weatherQuery =
+          await weatherRef
+              .where('ts', isEqualTo: weather.ts) // ตรวจสอบว่า timestamp ซ้ำไหม
+              .get();
+
+      if (weatherQuery.docs.isEmpty) {
+        // ไม่มีข้อมูล weather นี้ ซ้ำกันแล้ว
+        log("[weather.ts]: ${weather.ts}");
+        await weatherRef.add({
+          'hu': weather.hu,
+          'ic': weather.ic,
+          'pr': weather.pr,
+          'tp': weather.tp,
+          'wd': weather.wd,
+          'ws': weather.ws,
+          'ts': weather.ts,
+          'created_at': DateTime.now(),
+        });
+        log("[weather.ts][${value.city}]:✔️ ดึงและบันทึกข้อมูลเสร็จแล้ว");
+      } else {
+        log(
+          "[loadAndUpdate][${value.city}]: ❌ This weather information is duplicated.",
+        );
+      }
+    } catch (e, stackTrace) {
+      log('[loadAndUpdate]Error: $e\n$stackTrace');
+    }
   }
 }
--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_airnow/app/ui/home/controller/user_controller.dart';
 import 'package:flutter_airnow/app/ui/create/create_page.dart';
 import 'package:flutter_airnow/app/ui/home/controller/home_page_controller.dart';
-import 'package:flutter_airnow/app/ui/widget/custom_text.dart';
 import 'package:get/get.dart';
+import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,90 +19,124 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: homePageController.pageController.value,
-        onPageChanged: (index) {
-          homePageController.selectedIndex.value = index;
-        },
-        children: homePageController.widgetOptions,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          log("floatingActionButton: add()");
-          Get.to(() => CreatePage());
-        },
-        tooltip: 'Increment',
-        shape: const CircleBorder(),
-        backgroundColor: Theme.of(context).primaryColor,
-        child: Icon(
-          Icons.add,
-          color: Theme.of(context).appBarTheme.foregroundColor,
+    return SafeArea(
+      child: Scaffold(
+        body: PageView(
+          controller: homePageController.pageController.value,
+          onPageChanged: (index) {
+            homePageController.selectedIndex.value = index;
+          },
+          children: homePageController.widgetOptions,
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Obx(
-              () => IconButton(
-                onPressed: () {
-                  homePageController.changePage(0);
-                },
-                icon:
-                    homePageController.selectedIndex.value == 0
-                        ? Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Colors.white,
-                              child: Icon(Icons.home),
-                            ),
-                            SizedBox(width: 4),
-                            CustomText(
-                              text: "Home",
-                              size: 14,
-                              color: Colors.white,
-                            ),
-                          ],
-                        )
-                        : Center(child: Icon(Icons.home, color: Colors.white)),
-              ),
-            ),
-            SizedBox(width: MediaQuery.of(context).size.width * 0.3),
-            Obx(
-              () => IconButton(
-                onPressed: () {
-                  homePageController.changePage(1);
-                },
-                icon:
-                    homePageController.selectedIndex.value == 1
-                        ? Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CustomText(
-                              text: 'Profile',
-                              size: 14,
-                              color: Colors.white,
-                            ),
-                            SizedBox(width: 4),
-                            CircleAvatar(
-                              backgroundColor: Colors.white,
-                              child: Icon(Icons.person),
-                            ),
-                          ],
-                        )
-                        : Center(
-                          child: Icon(Icons.person, color: Colors.white),
-                        ),
-              ),
-            ),
-          ],
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            log("floatingActionButton: add()");
+            Get.to(() => CreatePage());
+          },
+          tooltip: 'Increment',
+          shape: const CircleBorder(),
+          backgroundColor: Theme.of(context).primaryColor,
+          child: Icon(
+            Icons.add,
+            color: Theme.of(context).appBarTheme.foregroundColor,
+          ),
         ),
+        bottomNavigationBar: Obx(() {
+          return StylishBottomBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            notchStyle: NotchStyle.circle,
+            option: DotBarOptions(dotStyle: DotStyle.circle),
+            items: [
+              BottomBarItem(
+                icon: const Icon(Icons.home),
+                title: const Text('Home'),
+                backgroundColor: Theme.of(context).appBarTheme.foregroundColor,
+              ),
+              BottomBarItem(
+                icon: const Icon(Icons.person),
+                title: const Text('Profile'),
+                backgroundColor: Theme.of(context).appBarTheme.foregroundColor,
+              ),
+            ],
+            hasNotch: true,
+            currentIndex: homePageController.selectedIndex.value,
+            onTap: (index) {
+              setState(() {
+                homePageController.selectedIndex.value = index;
+                homePageController.pageController.value.jumpToPage(index);
+              });
+            },
+
+            // fabLocation: StylishBarFabLocation.end,
+          );
+        }),
       ),
+
+      // bottomNavigationBar: BottomAppBar(
+      //   shape: const CircularNotchedRectangle(),
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Obx(
+      //         () => IconButton(
+      //           onPressed: () {
+      //             homePageController.changePage(0);
+      //           },
+      //           icon:
+      //               homePageController.selectedIndex.value == 0
+      //                   ? Row(
+      //                     mainAxisAlignment: MainAxisAlignment.center,
+      //                     children: [
+      //                       CircleAvatar(
+      //                         backgroundColor: Colors.white,
+      //                         child: Icon(Icons.home),
+      //                       ),
+      //                       SizedBox(width: 4),
+      //                       CustomText(
+      //                         text: "Home",
+      //                         size: 14,
+      //                         color: Colors.white,
+      //                       ),
+      //                     ],
+      //                   )
+      //                   : Center(
+      //                     child: Icon(Icons.home, color: Colors.white),
+      //                   ),
+      //         ),
+      //       ),
+      //       SizedBox(width: MediaQuery.of(context).size.width * 0.3),
+      //       Obx(
+      //         () => IconButton(
+      //           onPressed: () {
+      //             homePageController.changePage(1);
+      //           },
+      //           icon:
+      //               homePageController.selectedIndex.value == 1
+      //                   ? Row(
+      //                     mainAxisSize: MainAxisSize.min,
+      //                     mainAxisAlignment: MainAxisAlignment.center,
+      //                     children: [
+      //                       CustomText(
+      //                         text: 'Profile',
+      //                         size: 14,
+      //                         color: Colors.white,
+      //                       ),
+      //                       SizedBox(width: 4),
+      //                       CircleAvatar(
+      //                         backgroundColor: Colors.white,
+      //                         child: Icon(Icons.person),
+      //                       ),
+      //                     ],
+      //                   )
+      //                   : Center(
+      //                     child: Icon(Icons.person, color: Colors.white),
+      //                   ),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
